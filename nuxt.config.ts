4c99ba0836be18ddfd3edd9c6db94b8dc516880b
Mohammad Reza mg <mmg.workmail@gmail.com>
2025-05-17 11:57:41 +0300

    setup basic Nuxt 3 structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,27 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+
+  app: {
+    head: {
+      title: "Horse Racing Game",
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        }
+      ],
+      link: [
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
+        },
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@100..900&display=swap'
+        }
+      ]
+    }
+  },
+
 })
