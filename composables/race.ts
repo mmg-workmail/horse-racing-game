0884811b23794d2fdecc147151e9294a2c989e9c
Mohammad Reza mg <mmg.workmail@gmail.com>
2025-05-19 00:14:42 +0300

    setup for composables for horse and race

--- composables/race.ts
+++ composables/race.ts
@@ -1,17 +1,52 @@
-import { storeToRefs } from 'pinia';
-import { useRaceStore } from '~/stores/race';
+import { storeToRefs } from 'pinia'
+import { useRaceStore } from '~/stores/race'
 
 export const useRace = () => {
-    const raceStore = useRaceStore();
-    const { horses, schedule, results, currentRound } = storeToRefs(raceStore);
+    const raceStore = useRaceStore()
+
+    const {
+        getRaceSchedule,
+        getCurrentRound,
+        getIsRacing,
+        getIsPaused,
+        getGameStatus,
+        getRaceResults,
+        getCurrentRace,
+    } = storeToRefs(raceStore)
+
+    const generateRaceSchedule = () => {
+        raceStore.generateRaceSchedule()
+    }
+
+    const startRace = () => {
+        raceStore.startRace()
+    }
+
+    const pauseRace = () => {
+        raceStore.pauseRace()
+    }
+
+    const resumeRace = () => {
+        raceStore.resumeRace()
+    }
+
+    const resetGame = () => {
+        raceStore.resetGame()
+    }
 
     return {
-        horses,
-        schedule,
-        results,
-        currentRound,
-        generateHorses: raceStore.generateHorses,
-        generateSchedule: raceStore.generateSchedule,
-        startRace: raceStore.startRace
-    };
-};
+        raceSchedule: getRaceSchedule,
+        raceResults: getRaceResults,
+        currentRound: getCurrentRound,
+        isRacing: getIsRacing,
+        isPaused: getIsPaused,
+        gameStatus: getGameStatus,
+        currentRace: getCurrentRace,
+
+        generateRaceSchedule,
+        startRace,
+        pauseRace,
+        resumeRace,
+        resetGame
+    }
+}
