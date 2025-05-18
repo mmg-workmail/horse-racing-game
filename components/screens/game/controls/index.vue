560e1a2e1709d244b0298618f565027c22fca945
Mohammad Reza mg <mmg.workmail@gmail.com>
2025-05-19 02:56:52 +0300

    setup responsive

--- components/screens/game/controls/index.vue
+++ components/screens/game/controls/index.vue
@@ -15,15 +15,15 @@
 </script>
 
 <template>
-    <div class="flex justify-between  gap-2">
+    <div class="flex justify-between md:gap-2 gap-0.5">
         
-        <div class="flex items-center gap-2">
-            <h3 class="text-xl font-bold"> Current Round: {{ currentRound }} </h3>
+        <div class="flex items-center md:gap-2 gap-1">
+            <h3 class="md:text-xl text-xs font-bold"> Current Round: {{ currentRound }} </h3>
             <span> / </span>
-            <strong :class="{'text-red-600' : gameStatus === GameStatus.FINISHED}">{{ gameStatus }}</strong>
+            <strong class="md:text-base text-xs" :class="{'text-red-600' : gameStatus === GameStatus.FINISHED}">{{ gameStatus }}</strong>
         </div>
 
-        <div class="flex gap-2 justify-end">
+        <div class="flex md:gap-2 gap-1 justify-end">
             <ModulesBtn class="" @click="generateRaceSchedule" >Race Schedule</ModulesBtn>
 
             <ModulesBtn v-if="isRacing" @click="pauseRace" >Pause Race</ModulesBtn>
